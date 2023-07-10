43feb97d3f8f11b117e185ed56cc81309f6f9ab1
Silvio Ruiz <silviojrck@gmail.com>
2023-07-10 20:04:06 -0300

    Crea endpoint para crear tareas

--- src/controllers/tareas.controller.js
+++ src/controllers/tareas.controller.js
@@ -7,15 +7,22 @@ export const obtenerTareas = async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(400).json({
-            message: "Error al buscar las tareas"
+            message: 'Error al buscar las tareas',
         });
     }
 }
 
 export const crearTarea = async (req, res) => {
     try {
-
+        const tareaNueva = new Tarea(req.body);
+        await tareaNueva.save();
+        res.status(201).json({
+            message: 'La tarea se creó correctamente',
+        });
     } catch (err) {
-
+        console.log(err);
+        res.status(400).json({
+            message: 'Error al crear la tarea',
+        })
     }
 }
